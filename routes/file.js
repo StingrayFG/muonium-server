fb50db975fff67b5ddea1dd18644a3fcb80c121c
StingrayFG <54187585+StingrayFG@users.noreply.github.com>
2023-12-17 15:49:28 +0300

    Move drive and folder uuid checks to middleware functions

--- routes/file.js
+++ routes/file.js
@@ -27,8 +27,7 @@ const authenticateJWT = (req, res, next) => {
     const token = authHeader.split(' ')[1];
 
     jwt.verify(token, process.env.ACCESS_TOKEN_SECRET, (err, user) => {
-      if (err) { return res.sendStatus(403); }
-          
+      if (err) { return res.sendStatus(403); }       
       req.user = user;
       next();
     });
@@ -37,51 +36,76 @@ const authenticateJWT = (req, res, next) => {
   }
 };
 
-
-router.post('/file/upload', authenticateJWT, upload.single('file'), async function(req, res, next) {
-  let drive;  
-    
-  try {  
-    drive = await prisma.drive.findUnique({
-      where: {
-        uuid: req.body.driveUuid
+const checkUUIDs = async (req, res, next) => {
+  try {
+    let drive;
+    let folder;
+    await Promise.all([
+      drive = await prisma.drive.findUnique({
+        where: {
+          uuid: req.body.driveUuid
+        }
+      }),
+      async () => {
+        if (req.body.parentUuid) {
+          folder = await prisma.folder.findUnique({
+            where: {
+              uuid: req.body.parentUuid
+            }
+          })
+        }
+      }
+    ])
+    .then(() => {
+      if ((drive && folder) || (drive && !req.body.parentUuid)) { 
+        req.drive = drive;
+        next(); 
+      } else { 
+        res.sendStatus(404); 
       }
     })
   } catch (e) {
+    res.sendStatus(500);
+  }
+};
+
+const checkDriveSpace = async (req, res, next) => {
+  if ((req.drive.spaceUsed + req.file.size) < drive.spaceTotal) {
+    next();
+  } else {
+    res.sendStatus(500);
+  }
+};
+
+router.post('/file/upload', authenticateJWT, checkUUIDs, checkDriveSpace, upload.single('file'), async function(req, res, next) {
+  try {
+    let fileUuid = Buffer.from(crypto.randomUUID(), 'hex');
+    await Promise.all([
+      prisma.file.create({
+        data: {
+          name: req.file.filename,
+          uuid: fileUuid,
+          ownerUuid: req.user.uuid,
+          parentUuid: req.body.parentUuid,
+          driveUuid: req.body.driveUuid,
+        }
+      }),
+
+      prisma.drive.update({
+        where: {
+          uuid: req.body.driveUuid,
+        },
+        data: {
+          spaceUsed: { increment: req.file.size },
+        },
+      })
+    ])
+    .then(
+      res.sendStatus(201),
+    )
+  } catch (e) {
     res.sendStatus(409);
   }
-
-  if (drive) {
-    if ((drive.spaceUsed + req.file.size) < drive.spaceTotal) {
-      try {
-        await Promise.all([
-          prisma.file.create({
-            data: {
-              name: req.file.filename,
-              uuid: fileUuid,
-              ownerUuid: req.user.uuid,
-              parentUuid: req.body.parentUuid,
-              driveUuid: req.body.driveUuid,
-            }
-          }),
-
-          prisma.drive.update({
-            where: {
-              uuid: req.body.driveUuid,
-            },
-            data: {
-              spaceUsed: { increment: req.file.size },
-            },
-          })
-        ])
-        .then(
-          res.sendStatus(201),
-        )
-      } catch (e) {
-        res.sendStatus(409);
-      }
-    }
-  }
-
 });
+
   
