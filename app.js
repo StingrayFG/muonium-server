c6e41228991433c2db168f6ef51885b0a2f6acec
StingrayFG <54187585+StingrayFG@users.noreply.github.com>
2023-12-19 15:40:17 +0300

    Add routes to app.js

--- app.js
+++ app.js
@@ -4,8 +4,8 @@ var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
 
-var indexRouter = require('./routes/index');
-var usersRouter = require('./routes/users');
+var fileRouter = require('./routes/file');
+var folderRouter = require('./routes/folder');
 
 var app = express();
 
@@ -19,8 +19,8 @@ app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
-app.use('/', indexRouter);
-app.use('/users', usersRouter);
+app.use('/', fileRouter);
+app.use('/', folderRouter);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
