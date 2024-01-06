971eab3b2ff2f3ad03cc937232c0d19197f21eaf
StingrayFG <54187585+StingrayFG@users.noreply.github.com>
2024-01-06 16:02:29 +0300

    Fix multiple responses being sent

--- routes/auth.js
+++ routes/auth.js
@@ -35,46 +35,48 @@ router.post('/auth/login', async function(req, res, next) {
     }
   }
   
-  await getUser()
-  .then(async result => (await getDrive(result)))
-  .then(() => {
-    if (user && drive) {
-      const accessToken = jwt.sign({ login: user.login }, process.env.ACCESS_TOKEN_SECRET);
-      res.send({accessToken, userUuid: user.uuid, driveUuid: drive.uuid});
-    } else {
-      res.sendStatus(404);
-    }  
-  })
-  .catch(
-    res.sendStatus(409)
-  )  
+  try {
+    await getUser()
+    .then(async result => (await getDrive(result)))
+    .then(() => {
+      if (user && drive) {
+        const accessToken = jwt.sign({ login: user.login }, process.env.ACCESS_TOKEN_SECRET);
+        return res.send({login: user.login, accessToken, userUuid: user.uuid, driveUuid: drive.uuid});
+      } else {
+        return res.sendStatus(404);
+      }  
+    })
+  } catch (e) {
+    return res.sendStatus(404);
+  }
 });
 
 router.post('/auth/signup', async function(req, res, next) {
-  let userUuid = crypto.randomUUID();
-  Promise.all([
-    await prisma.user.create({
-      data: {
-        uuid: userUuid,
-        login: req.body.userData.login,
-        password: req.body.userData.password,
-      },
-    }),
-    await prisma.drive.create({
-      data: {
-        uuid: crypto.randomUUID(),
-        ownerUuid: userUuid,
-        spaceTotal: 1024 * 1024 * 100,
-        spaceUsed: 0,
-      },
+  try {
+    let userUuid = crypto.randomUUID();
+    Promise.all([
+      await prisma.user.create({
+        data: {
+          uuid: userUuid,
+          login: req.body.userData.login,
+          password: req.body.userData.password,
+        },
+      }),
+      await prisma.drive.create({
+        data: {
+          uuid: crypto.randomUUID(),
+          ownerUuid: userUuid,
+          spaceTotal: 1024 * 1024 * 100,
+          spaceUsed: 0,
+        },
+      })
+    ])
+    .then(() => {
+      return res.sendStatus(201)
     })
-  ])
-  .then(
-    res.sendStatus(201)
-  )
-  .catch(
-    res.sendStatus(409)
-  )  
+  } catch (e) {
+    return res.sendStatus(404);
+  }
 });
 
 module.exports = router;
