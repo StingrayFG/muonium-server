31eee6b19de6c7de735c72d37372676c76274090
StingrayFG <54187585+StingrayFG@users.noreply.github.com>
2024-01-07 09:09:38 +0300

    Modify files upload and download
    Add two-step file download by token, add pre-upload file size and drive space checks.

--- routes/file.js
+++ routes/file.js
@@ -23,136 +23,154 @@ var upload = multer({ storage: storage });
 
 const authenticateJWT = (req, res, next) => {
   const authHeader = req.headers.authorization;
-
   if (authHeader) {
     const token = authHeader.split(' ')[1];
-
     jwt.verify(token, process.env.ACCESS_TOKEN_SECRET, (err, user) => {
-      if (err) { return res.sendStatus(403); }       
-      req.user = user;
-      next();
+      if (err) { return res.sendStatus(403); }   
+      else if (user.uuid != req.body.userUuid) { return res.sendStatus(403); }   
+      else { req.user = user; next(); } 
     });
   } else {
-    res.sendStatus(401);
+    return res.sendStatus(401);
   }
 };
 
+const parseSize = async (req, res, next) => {
+  req.body = req.params;
+  req.file = { size: parseInt(req.headers['content-length']) };
+  next();
+}
+
 const checkDrive = async (req, res, next) => {
-  try {
-    const drive = await prisma.drive.findUnique({
-      where: {
-        uuid: req.body.driveUuid,
-      }
-    })
-    .then(() => {
-      if (drive) {
-        req.drive = drive;
-        next(); 
-      }
-    })
-  } catch (e) {
-    res.sendStatus(404);
-  }
+  console.log('checkDrive');
+  await prisma.drive.findUnique({
+    where: {
+      uuid: req.body.driveUuid,
+    }
+  })
+  .then(result => {
+    req.drive = result;
+    next();
+  })
+  .catch(() => {
+    return res.sendStatus(404);
+  })
 };
 
 const checkDriveSpace = async (req, res, next) => {
-  if ((req.drive.spaceUsed + req.file.size) < drive.spaceTotal) {
+  console.log('checkDriveSpace');
+  if ((req.drive.spaceUsed + req.file.size) < req.drive.spaceTotal) {
     next();
   } else {
-    res.sendStatus(404);
+    return res.sendStatus(404);
   }
 };
 
 const checkParentFolder = async (req, res, next) => {
-  try {
-    if ((req.body.parentUuid == '/root') || (req.body.parentUuid == '/trash')) {
-      next();
-    } else if (req.body.parentUuid) {
-      const folder = await prisma.folder.findUnique({
-        where: {
-          uuid: req.body.parentUuid,
-        }
-      })
-      .then(() => {
-          if (folder) {
-            next();
-          }
-        }
-      )
-    }
-  } catch (e) {
-    res.sendStatus(404);
+  console.log('checkParentFolder');
+  if ((req.body.parentUuid === 'root') || (req.body.parentUuid === 'trash')) {
+    next();
+  } else if (req.body.parentUuid) {
+    await prisma.folder.findUnique({
+      where: {
+        uuid: req.body.parentUuid,
+      }
+    })
+    .then(result => {
+      if (result) {
+        next();
+      }
+    })
+    .catch(() => {
+      return res.sendStatus(404);
+    })
   }
 };
 
 const checkFile = async (req, res, next) => {
-  try {
-    const file = await prisma.folder.findUnique({
-      where: {
-        uuid: req.body.fileUuid,
-      }
-    })
-    .then(() => {
-        if (file) {
-          req.body.file = file;
-          next();
-        }
-      }
-    )
-  } catch (e) {
-    res.sendStatus(404);
-  }
+  console.log('checkFile');
+  await prisma.file.findUnique({
+    where: {
+      uuid: req.body.fileUuid,
+    }
+  })
+  .then(result => {
+    if (result) {
+      req.body.file = result;
+      next();
+    }
+  })
+  .catch(() => {
+    return res.sendStatus(404);
+  })
 };
 
 // Endpoints
-router.post('/file/upload', authenticateJWT, checkDrive, checkDriveSpace, checkParentFolder, upload.single('file'), async function(req, res, next) {
-  try {
-    await prisma.file.create({
-      data: {
-        name: req.file.fileName,
-        uuid: Buffer.from(crypto.randomUUID(), 'hex'),
-        size: req.file.size,
-        ownerUuid: req.user.uuid,
-        parentUuid: req.body.parentUuid,
-        driveUuid: req.body.driveUuid,
-      }
-    })
-    .then(
-      await prisma.drive.update({
-        where: {
-          uuid: req.body.driveUuid,
-        },
-        data: {
-          spaceUsed: { increment: req.file.size },
-        },
-      })
-      .then(
-        res.sendStatus(201),
-      )
-    )  
-  } catch (e) {
-    res.sendStatus(404);
-  }
-});
-
-router.get('/file/download/:uuid', authenticateJWT, checkDrive, checkParentFolder, checkFile, async function(req, res, next) {
-  try {
-    const file = await prisma.file.findUnique({
+router.post('/file/upload/:userUuid/:driveUuid/:parentUuid', parseSize, authenticateJWT, 
+checkDrive, checkDriveSpace, checkParentFolder, upload.single('file'), async function(req, res, next) {
+  await prisma.file.create({
+    data: {
+      name: req.file.filename,
+      uuid: crypto.randomUUID(),
+      size: req.file.size,
+      ownerUuid: req.params.userUuid,
+      parentUuid: req.params.parentUuid,
+      driveUuid: req.params.driveUuid,
+    }
+  })
+  .then(
+    await prisma.drive.update({
       where: {
-        uuid: req.params.uuid,
-      }
+        uuid: req.params.driveUuid,
+      },
+      data: {
+        spaceUsed: { increment: req.file.size },
+      },
     })
     .then(() => {
-      if (file) {
-        res.set('Content-Disposition', `attachment; filename="${path.parse(file.name).name}"`);
-        res.sendFile(file.name, { root: 'uploads/'});
-      } else {
-        res.status(404);
-      }
+      return res.sendStatus(201)
     })
-  } catch (e) {
-    res.sendStatus(404);
-  }
+    .catch((e) => {
+      return res.sendStatus(404);
+    })
+  ) 
+  .catch((e) => {
+    return res.sendStatus(404);
+  }) 
+});
+
+router.post('/file/download', authenticateJWT, checkDrive, checkFile, async function(req, res, next) {
+  const downloadToken = jwt.sign({ uuid: req.body.fileUuid }, process.env.ACCESS_TOKEN_SECRET);
+  res.send({ downloadToken });
+});
+
+router.get('/file/download/:uuid/:token', async function(req, res, next) {
+  jwt.verify(req.params.token, process.env.ACCESS_TOKEN_SECRET, async (err, file) => {
+    console.log(file);
+    if (err) { console.log(1); return res.sendStatus(403); }   
+    else if (file.uuid != req.params.uuid) { console.log(2); return res.sendStatus(403); }   
+    else if ((Math.floor(Date.now() / 1000) - file.iat) > 300) { console.log(3); return res.sendStatus(410); } 
+    else {
+      console.log(4); 
+      await prisma.file.findUnique({
+        where: {
+          uuid: req.params.uuid,
+        }
+      })
+      .then(result => {
+        console.log(result)
+        if (result) {
+          res.set('Content-Disposition', `attachment; filename="${path.parse(result.name).name}"`);
+          res.sendFile(result.name, { root: 'uploads/'});
+        } else {
+          res.status(404);
+        }
+      })
+      .catch(() => {
+        return res.sendStatus(404);
+      })
+    } 
+  });
 });
 
 router.put('/file/rename', authenticateJWT, checkDrive, checkParentFolder, checkFile, async function(req, res, next) {
@@ -166,7 +184,7 @@ router.put('/file/rename', authenticateJWT, checkDrive, checkParentFolder, check
       },
     })
     .then(
-      res.sendStatus(200),
+      res.sendStatus(200)
     )
   } catch (e) {
     res.sendStatus(404);
@@ -189,7 +207,7 @@ router.put('/file/copy', authenticateJWT, checkDrive, checkParentFolder, checkFi
         },
       })
       .then(
-        res.sendStatus(201),
+        res.sendStatus(201)
       )
     )    
   } catch (e) {
@@ -208,7 +226,7 @@ router.put('/file/move', authenticateJWT, checkDrive, checkParentFolder, checkFi
       },
     })
     .then(
-      res.sendStatus(200),
+      res.sendStatus(200)
     )
   } catch (e) {
     res.sendStatus(404);
@@ -217,7 +235,7 @@ router.put('/file/move', authenticateJWT, checkDrive, checkParentFolder, checkFi
 
 router.put('/file/remove', authenticateJWT, checkDrive, checkParentFolder, checkFile, async function(req, res, next) {
   try {
-    await prisma.drive.update({
+    await prisma.file.update({
       where: {
         uuid: req.body.file.uuid,
       },
@@ -226,7 +244,7 @@ router.put('/file/remove', authenticateJWT, checkDrive, checkParentFolder, check
       },
     })
     .then(
-      res.sendStatus(200),
+      res.sendStatus(200)
     )
   } catch (e) {
     res.sendStatus(404);
@@ -250,7 +268,7 @@ router.delete('/file/delete', authenticateJWT, checkDrive, checkParentFolder, ch
         },
       })
       .then(
-        res.sendStatus(200),
+        res.sendStatus(200)
       )
     )  
   } catch (e) {
