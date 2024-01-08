7767dd84a329d765385aeb0fb79baf4768a92237
StingrayFG <54187585+StingrayFG@users.noreply.github.com>
2024-01-08 10:18:38 +0300

    Refactor most folder endpoints, add modificationDate field
    Remove try - catch blocks, add return keyword to res.send calls, add recover (from trash) endpoint

--- app.js
+++ app.js
@@ -5,14 +5,12 @@ var cookieParser = require('cookie-parser');
 var logger = require('morgan');
 var cors = require('cors')
 
-var indexRouter = require('./routes/index');
+//var indexRouter = require('./routes/index');
 var authRouter = require('./routes/auth');
-//var fileRouter = require('./routes/file');
-//var folderRouter = require('./routes/folder');
+var fileRouter = require('./routes/file');
+var folderRouter = require('./routes/folder');
 
 var app = express();
-
-// view engine setup
 
 app.use(cors({
   origin: [,
@@ -20,7 +18,7 @@ app.use(cors({
   ]
 }))
 
-
+// view engine setup
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'jade');
 
@@ -30,25 +28,9 @@ app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
-app.use('/', indexRouter);
+//app.use('/', indexRouter);
 app.use('/', authRouter);
-//app.use('/', fileRouter);
-//app.use('/', folderRouter);
-
-// catch 404 and forward to error handler
-app.use(function(req, res, next) {
-  next(createError(404));
-});
-
-// error handler
-app.use(function(err, req, res, next) {
-  // set locals, only providing error in development
-  res.locals.message = err.message;
-  res.locals.error = req.app.get('env') === 'development' ? err : {};
-
-  // render the error page
-  res.status(err.status || 500);
-  res.render('error');
-});
+app.use('/', fileRouter);
+app.use('/', folderRouter);
 
 module.exports = app;
--- routes/folder.js
+++ routes/folder.js
@@ -23,140 +23,243 @@ var upload = multer({ storage: storage });
 
 const authenticateJWT = (req, res, next) => {
   const authHeader = req.headers.authorization;
-
   if (authHeader) {
     const token = authHeader.split(' ')[1];
-
     jwt.verify(token, process.env.ACCESS_TOKEN_SECRET, (err, user) => {
-      if (err) { return res.sendStatus(403); }       
-      req.user = user;
-      next();
+      if (err) { return res.sendStatus(403); }   
+      else if (user.uuid != req.body.userUuid) { return res.sendStatus(403); }   
+      else { req.user = user; next(); } 
     });
   } else {
-    res.sendStatus(401);
+    return res.sendStatus(401);
   }
 };
 
+const parseSize = async (req, res, next) => {
+  req.body = req.params;
+  req.file = { size: parseInt(req.headers['content-length']) };
+  next();
+}
+
 const checkDrive = async (req, res, next) => {
-  try {
-    const drive = await prisma.drive.findUnique({
-      where: {
-        uuid: req.body.driveUuid,
-      }
-    })
-    .then(() => {
-      if (drive) {
-        req.drive = drive;
-        next(); 
-      }
-    })
-  } catch (e) {
-    res.sendStatus(404);
-  }
+  console.log('checkDrive');
+  await prisma.drive.findUnique({
+    where: {
+      uuid: req.body.driveUuid,
+    }
+  })
+  .then(result => {
+    req.drive = result;
+    next();
+  })
+  .catch(() => {
+    return res.sendStatus(404);
+  })
 };
 
 const checkParentFolder = async (req, res, next) => {
-  try {
-    if ((req.body.parentUuid == '/root') || (req.body.parentUuid == '/trash')) {
-      next();
-    } else if (req.body.parentUuid) {
-      const folder = await prisma.folder.findUnique({
-        where: {
-          uuid: req.body.parentUuid,
-        }
-      })
-      .then(() => {
-          if (folder) {
-            next();
-          }
-        }
-      )
-    }
-  } catch (e) {
-    res.sendStatus(404);
+  console.log('checkParentFolder');
+  if ((req.body.parentUuid == 'root') || (req.body.parentUuid == 'trash')) {
+    next();
+  } else if (req.body.parentUuid) {
+    await prisma.folder.findUnique({
+      where: {
+        uuid: req.body.parentUuid,
+      }
+    })
+    .then(result => {
+      if (result) {
+        next();
+      }
+    })
+    .catch(() => {
+      return res.sendStatus(404);
+    })
   }
 };
 
 const checkFolder = async (req, res, next) => {
-  try {
-    const file = await prisma.folder.findUnique({
-      where: {
-        uuid: req.body.fileUuid,
-      }
-    })
-    .then(() => {
-        if (file) {
-          req.body.file = file;
-          next();
-        }
-      }
-    )
-  } catch (e) {
-    res.sendStatus(404);
-  }
+  console.log('checkFolder');
+  await prisma.folder.findUnique({
+    where: {
+      uuid: req.body.folderUuid,
+    }
+  })
+  .then(result => {
+    if (result) {
+      req.folder = result;
+      next();
+    }
+  })
+  .catch(() => {
+    return res.sendStatus(404);
+  })
 };
 
 // Endpoints
-router.post('/folder/create', authenticateJWT, checkDrive, checkParentFolder, checkFolder, async function(req, res, next) {
-  try {
-    await prisma.folder.create({
-      data: {
-        name: req.file.folderName,
-        uuid: Buffer.from(crypto.randomUUID(), 'hex'),
-        ownerUuid: req.user.uuid,
-        parentUuid: req.body.parentUuid,
-        driveUuid: req.body.driveUuid,
-      }
-    })
-    .then(
-      res.sendStatus(201),
-    )  
-  } catch (e) {
-    res.sendStatus(404);
-  }
+router.post('/folder/create', authenticateJWT, checkDrive, checkParentFolder, async function(req, res, next) {
+  await prisma.folder.create({
+    data: {
+      name: req.body.folderName,
+      uuid: crypto.randomUUID(),
+      ownerUuid: req.user.uuid,
+      parentUuid: req.body.parentUuid,
+      driveUuid: req.body.driveUuid,
+    }
+  })
+  .then(() => {
+    return res.sendStatus(201);
+  }) 
+  .catch((e) => {
+    console.log(e)
+    return res.sendStatus(404);
+  }) 
 });
 
-router.put('/folder/rename', authenticateJWT, checkDrive, checkParentFolder, checkFolder, async function(req, res, next) {
-  try {
-    await prisma.folder.update({
-      where: {
-        uuid: req.body.folderUuid,
-      },
-      data: {
-        name: req.body.folderName,
-      },
-    })
-    .then(
-      res.sendStatus(200),
-    )
-  } catch (e) {
-    res.sendStatus(404);
-  }
-});
-
-router.put('/folder/move', authenticateJWT, checkDrive, checkParentFolder, checkFolder, async function(req, res, next) {
-  try {
-    await prisma.folder.findUnique({
-      where: {
-        uuid: req.body.folderUuid,
-      }
-    })
-    .then(
-      await prisma.file.update({
+router.post('/folder/get', authenticateJWT, checkDrive, checkParentFolder, async function(req, res, next) {
+  let folder = {files: [], folders: []};
+  if (req.body.parentUuid === 'trash') {
+    Promise.all([
+      folder.files = await prisma.file.findMany({
+        orderBy: [
+          {
+            name: 'asc',
+          },
+        ],
         where: {
-          uuid: req.body.folderUuid,
+          ownerUuid: req.body.userUuid,
+          isRemoved: true,
         },
-        data: {
-          name: req.body.newParentUuid,
+      }),
+      folder.folders = await prisma.folder.findMany({
+        orderBy: [
+          {
+            name: 'asc',
+          },
+        ],
+        where: {
+          ownerUuid: req.body.userUuid,
+          isRemoved: true,
         },
-      })
-      .then(
-        res.sendStatus(200),
-      )
-    )
-  } catch (e) {
-    res.sendStatus(404);
+      }),
+    ])
+    .then(() => {
+      folder.files.forEach(file => {
+        file.name = path.parse(file.name).name;
+      });
+      return res.send(folder);
+    }) 
+    .catch(() => {
+      return res.sendStatus(404);
+    }) 
+  } else {
+    Promise.all([
+      folder.files = await prisma.file.findMany({
+        orderBy: [
+          {
+            name: 'asc',
+          },
+        ],
+        where: {
+          parentUuid: req.body.parentUuid,
+          ownerUuid: req.body.userUuid,
+          isRemoved: false,
+        },
+      }),
+      folder.folders = await prisma.folder.findMany({
+        orderBy: [
+          {
+            name: 'asc',
+          },
+        ],
+        where: {
+          parentUuid: req.body.parentUuid,
+          ownerUuid: req.body.userUuid,
+          isRemoved: false,
+        },
+      }),
+    ])
+    .then(() => {
+      folder.files.forEach(file => {
+        file.name = path.parse(file.name).name;
+      });
+      return res.send(folder);
+    })   
+    .catch(() => {
+      return res.sendStatus(404);
+    }) 
   }
+});
+
+router.put('/folder/rename', authenticateJWT, checkDrive, checkFolder, async function(req, res, next) {
+  modificationDate = Date.now();
+
+  await prisma.folder.update({
+    where: {
+      uuid: req.folder.uuid,
+    },
+    data: {
+      name: req.body.folderName + '.' + modificationDate,
+      modificationDate: new Date(modificationDate),
+    },
+  })
+  .then(() => {
+    return res.sendStatus(200);
+  })
+  .catch(() => {
+    return res.sendStatus(404);
+  })
+});
+
+router.put('/folder/move', authenticateJWT, checkDrive, checkFolder, async function(req, res, next) {
+  await prisma.folder.update({
+    where: {
+      uuid: req.folder.uuid,
+    },
+    data: {
+      parentUuid: req.body.parentUuid,
+    },
+  })
+  .then(() => {
+    return res.sendStatus(200)
+  })
+  .catch(() => {
+    return res.sendStatus(404);
+  })
+});
+
+router.put('/folder/remove', authenticateJWT, checkDrive, checkFolder, async function(req, res, next) {
+  await prisma.folder.update({
+    where: {
+      uuid: req.folder.uuid,
+    },
+    data: {
+      isRemoved: true,
+    },
+  })
+  .then(() => {
+    return res.sendStatus(200)
+  })
+  .catch(() => {
+    return res.sendStatus(404);
+  })
+});
+
+router.put('/folder/recover', authenticateJWT, checkDrive, checkFolder, async function(req, res, next) {
+  await prisma.folder.update({
+    where: {
+      uuid: req.folder.uuid,
+    },
+    data: {
+      isRemoved: false,
+    },
+  })
+  .then(() => {
+    return res.sendStatus(200)
+  })
+  .catch(() => {
+    return res.sendStatus(404);
+  })
 });
 
 router.delete('/folder/delete', authenticateJWT, checkDrive, checkParentFolder, checkFolder, async function(req, res, next) {
@@ -168,8 +271,7 @@ router.delete('/folder/delete', authenticateJWT, checkDrive, checkParentFolder, 
     })
     .then(() => {
       deleteChildren();
-      res.sendStatus(200);
-      
+      res.sendStatus(200);      
     })
   } catch (e) {
     res.sendStatus(404);
@@ -235,9 +337,9 @@ router.delete('/folder/delete', authenticateJWT, checkDrive, checkParentFolder, 
         },
 
       ])   
-      .then(() => {
-        deletedParentUuids = nextDeletedParentUuids;
-      })
+      .then(
+        deletedParentUuids = nextDeletedParentUuids
+      )
     };
   };
 });
