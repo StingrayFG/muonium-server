5a3e453b8504348aab46a17eb4db9c8538d5f1d5
StingrayFG <54187585+StingrayFG@users.noreply.github.com>
2024-01-17 12:58:39 +0300

    Add jwt auth for /drive/get endpoint

--- routes/drive.js
+++ routes/drive.js
@@ -7,10 +7,25 @@ var router = express.Router();
 const { PrismaClient } = require('@prisma/client')
 const prisma = new PrismaClient()
 
-router.post('/drive/get', async function(req, res, next) {
+// Middleware
+const authenticateJWT = (req, res, next) => {
+  const authHeader = req.headers.authorization;
+  if (authHeader) {
+    const token = authHeader.split(' ')[1];
+    jwt.verify(token, process.env.ACCESS_TOKEN_SECRET, (err, user) => {
+      if (err) { return res.sendStatus(403); }   
+      else if (user.uuid != req.body.userUuid) { return res.sendStatus(403); }   
+      else { req.user = user; next(); } 
+    });
+  } else {
+    return res.sendStatus(401);
+  }
+};
+
+router.post('/drive/get', authenticateJWT, async function(req, res, next) {
   await prisma.drive.findUnique({
     where: {
-      uuid: req.body.uuid,
+      uuid: req.body.driveUuid,
     }
   })
   .then(result => {
