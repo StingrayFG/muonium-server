5da9ec209b3d33813ecd686587d4990b4c47ba7a
StingrayFG <54187585+StingrayFG@users.noreply.github.com>
2024-01-23 02:33:43 +0300

    Update auth to work with bcrypt package

--- routes/auth.js
+++ routes/auth.js
@@ -1,5 +1,6 @@
 var jwt = require('jsonwebtoken');
 var crypto = require('crypto');
+var bcrypt = require('bcrypt');
 
 var express = require('express');
 var router = express.Router();
@@ -12,32 +13,50 @@ router.post('/auth/login', async function(req, res, next) {
   let drive;
 
   const getUser = async () => {
-    result = await prisma.user.findUnique({
-      where: {
-        login: req.body.login,
-        password: req.body.password,
-      }
-    })
-    user = result;
-    return result;
-  }
-  const getDrive = async (u) => {
-    if (u) {
-      result = await prisma.drive.findFirst({
+    return new Promise( async function(resolve, reject) {
+      await prisma.user.findUnique({
         where: {
-          ownerUuid: u.uuid,
+          login: req.body.login,
         }
       })
-      drive = result;
-      return result;
-    } else {
-      return null;
-    }
+      .then(result => {
+        if (bcrypt.compareSync(req.body.password, result.password)) {
+          console.log(result)
+          user = result;
+          resolve(result);
+        } else {
+          reject();
+        }
+      })
+    })
+  }
+
+  const getDrive = async (u) => {
+    return new Promise( async function(resolve, reject) {
+      if (u) {
+        result = await prisma.drive.findFirst({
+          where: {
+            ownerUuid: u.uuid,
+          }
+        })
+        .then(result => {
+          if (result) {
+            drive = result;
+            resolve(result);
+          } else {
+            reject();
+          }
+        })
+      }
+    })
   }
   
   try {
     await getUser()
-    .then(async result => (await getDrive(result)))
+    .then(async result => {
+      console.log(result)
+      await getDrive(result)
+    })
     .then(() => {
       if (user && drive) {
         const accessToken = jwt.sign({ uuid: user.uuid }, process.env.ACCESS_TOKEN_SECRET);
@@ -57,12 +76,16 @@ router.post('/auth/login', async function(req, res, next) {
 router.post('/auth/signup', async function(req, res, next) {
   try {
     let userUuid = crypto.randomUUID();
+
+    const saltRounds = 10;
+    const hashedPassword = bcrypt.hashSync(req.body.password, saltRounds);
+
     Promise.all([
       await prisma.user.create({
         data: {
           uuid: userUuid,
           login: req.body.login,
-          password: req.body.password,
+          password: hashedPassword,
         },
       }),
       await prisma.drive.create({
