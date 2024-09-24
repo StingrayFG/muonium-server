1a29a8824f6685e7a9864139f1bd54feabb2bf5b
StingrayFG <54187585+StingrayFG@users.noreply.github.com>
2024-09-24 18:12:38 +0300

    Update: refactor auth route

--- routes/auth.js
+++ routes/auth.js
@@ -1,140 +1,235 @@
-var jwt = require('jsonwebtoken');
-var crypto = require('crypto');
-var bcrypt = require('bcrypt');
-
-var express = require('express');
-var router = express.Router();
-
-const { PrismaClient } = require('@prisma/client')
-const prisma = new PrismaClient()
-
+const express = require('express');
+const jwt = require('jsonwebtoken');
+const crypto = require('crypto');
+const bcrypt = require('bcrypt');
 const redis = require('redis');
-const client = redis.createClient({
-  url: process.env.REDIS_URL,
-})
+const { PrismaClient } = require('@prisma/client')
+
+
+const router = express.Router();
+const prisma = new PrismaClient()
+const client = redis.createClient({ url: process.env.REDIS_URL })
 client.connect().catch(console.error)
 
-// Handle login
-router.post('/auth/login', async function(req, res, next) {
-  // Search for a login attempt from request's origin ip record 
-  const loginAttempt = await client.get(req.headers['x-forwarded-for'] + '-login'); 
-  console.log(loginAttempt)
 
-  if (!loginAttempt) {
-    // Create a new redis record with ttl=30s to limit login requests to one per 30s
-    client.set(req.headers['x-forwarded-for'] + '-login', ' ');
-    client.expire(req.headers['x-forwarded-for'] + '-login', 10); 
+// MIDDLEWARE
+const validateUserData = async (req, res, next) => {
+  let isUserDataPresent = false;
+  let isUserDataValidAsObject = false;
+  let isLoginPresent = false;
+  let isPasswordPresent = false;
 
-    try {
-      let user;
-      let drive;
-    
-      const getUser = async () => {
-        return new Promise( async function(resolve, reject) {
-          await prisma.user.findUnique({ // Find a user based on login in request
-            where: {
-              login: req.body.login,
-            }
-          })
-          .then(result => {
-            if (result) { 
-              if (bcrypt.compareSync(req.body.password, result.password)) { // Compare password in request with the found user's password hashsum
-                user = result;
-                resolve(result);
-              } else {
-                reject();
-              }
-            } else {
-              reject();
-            }
-          })
-        })
+  const checkUserData = () => {
+    if ('userData' in req.body) {
+      isUserDataPresent = true;
+      if (typeof req.body.userData === 'object') { 
+        isUserDataValidAsObject = true;
+        checkLogin();
+        checkPassword();
       }
-    
-      const getDrive = async (u) => {
-        return new Promise( async function(resolve, reject) { // Find the found user's drive
-          if (u) {
-            result = await prisma.drive.findFirst({
-              where: {
-                ownerUuid: u.uuid,
-              }
-            })
-            .then(result => {
-              if (result) {
-                drive = result;
-                resolve(result);
-              } else {
-                reject();
-              }
-            })
-          }
-        })
+    }
+  }
+
+  const checkLogin = () => {
+    if ('login' in req.body.userData) {
+      if (req.body.userData.login) {
+        isLoginPresent = true;
       }
-      
-      await getUser()
-      .then(async result => {
-        await getDrive(result)
+    }
+  }
+
+  const checkPassword = () => {
+    if ('password' in req.body.userData) {
+      if (req.body.userData.password) {
+        isPasswordPresent = true;
+      }
+    }
+  }
+
+  checkUserData();
+
+  if (!isUserDataPresent || !isUserDataValidAsObject) {
+    return res.sendStatus(422);
+  } else if (!isLoginPresent || !isPasswordPresent) {
+    return res.sendStatus(400);
+  } else {
+    next();
+  }
+};
+
+
+// ENDPOINTS
+router.post('/auth/login', validateUserData, async function(req, res, next) {
+
+  const getUser = async () => {
+    return new Promise( async function(resolve, reject) {
+      await prisma.user.findUnique({
+        where: {
+          login: req.body.userData.login,
+        }
       })
-      .then(() => {
-        if (user && drive) {
-          const accessToken = jwt.sign({ uuid: user.uuid }, process.env.ACCESS_TOKEN_SECRET); // Sign jwt if both user and drive have been found
-          return res.send({ userUuid: user.uuid, login: user.login, accessToken, driveUuid: drive.uuid });
+      .then(user => {
+        if (user) { 
+          if (bcrypt.compareSync(req.body.userData.password, user.password)) { 
+            resolve(user);
+          } else {
+            reject();
+          }
         } else {
-          return res.sendStatus(404);
-        }  
+          reject();
+        }
       })
       .catch(() => {
-        return res.sendStatus(404)
-      })  
-    } catch {
-      return res.sendStatus(500);
-    }
+        reject();
+      })
+    })
+  }
+
+  const getDrive = async (user) => {
+    return new Promise( async function(resolve, reject) {
+      if (user) {
+        result = await prisma.drive.findFirst({
+          where: {
+            ownerUuid: user.uuid,
+          }
+        })
+        .then(drive => {
+          if (drive) {
+            resolve(drive);
+          } else {
+            reject();
+          }
+        })
+        .catch(() => {
+          reject();
+        })
+      }
+    })
+  }
+
+  const getAll = async () => {
+    return new Promise( async function(resolve, reject) {
+      await getUser()
+      .then(async user => {
+        await getDrive(user)
+        .then(async drive => {
+          if (user && drive) {
+            resolve({ 
+              userData: {
+                uuid: user.uuid,
+                login: user.login,
+                accessToken: jwt.sign({ uuid: user.uuid }, process.env.ACCESS_TOKEN_SECRET)
+              }, 
+              driveData: {
+                uuid: drive.uuid
+              }
+            });
+          } else {
+            reject();
+          }  
+        })
+      })
+      .catch(() => {
+        reject();
+      })          
+    })
+  }
+  
+
+  const loginAttempt = await client.get(req.headers['x-forwarded-for'] + '-login'); 
+
+  if (!loginAttempt) {
+    client.set(req.headers['x-forwarded-for'] + '-login', ' ');
+    client.expire(req.headers['x-forwarded-for'] + '-login', 3); 
+
+    await getAll()
+    .then(data => {
+      return res.send(data);
+    })
+    .catch(() => {
+      return res.sendStatus(404);
+    })
   } else {
     return res.sendStatus(423);
   }
 });
 
-router.post('/auth/signup', async function(req, res, next) {
-  // Search for a signup attempt from request's origin ip record 
+
+router.post('/auth/signup', validateUserData, async function(req, res, next) {
+
+  const createUser = async () => {
+    return new Promise(async function(resolve, reject) {
+      const userUuid = crypto.randomUUID();
+      const saltRounds = 1000;
+      const hashedPassword = bcrypt.hashSync(req.body.userData.password, saltRounds);
+
+      await prisma.user.create({
+        data: {
+          uuid: userUuid,
+          login: req.body.userData.login,
+          password: hashedPassword
+        }
+      })
+      .then(user => {
+        resolve(user);
+      })
+      .catch(() => {
+        reject();
+      })
+    })
+  }
+
+  const createDrive = async (user) => {
+    return new Promise(async function(resolve, reject) {
+      await prisma.drive.create({
+        data: {
+          uuid: crypto.randomUUID(),
+          ownerUuid: user.uuid,
+          spaceTotal: 1024 * 1024 * process.env.NEW_DRIVE_SIZE,
+          spaceUsed: 0,
+        },
+      })
+      .then(drive => {
+        resolve(drive);
+      })
+      .catch(() => {
+        reject();
+      })
+    })
+  }
+
+  const createAll = async () => {
+    return new Promise(async function(resolve, reject) {
+      await createUser()
+      .then(async user => {
+        await createDrive(user)
+        .then(async drive => {
+          if (user && drive) {
+            resolve();
+          } else {
+            reject();
+          }   
+        })
+      })
+      .catch(() => {
+        reject();
+      })
+    })
+  }
+
+
   const signupAttempt = await client.get(req.headers['x-forwarded-for'] + '-signup'); 
 
   if (!signupAttempt) {
-    let userUuid = crypto.randomUUID();
-
-    const saltRounds = 10;
-    const hashedPassword = bcrypt.hashSync(req.body.password, saltRounds);
-
-    try {
-      Promise.all([
-        await prisma.user.create({
-          data: {
-            uuid: userUuid,
-            login: req.body.login,
-            password: hashedPassword,
-          },
-        }),
-        await prisma.drive.create({
-          data: {
-            uuid: crypto.randomUUID(),
-            ownerUuid: userUuid,
-            spaceTotal: 1024 * 1024 * process.env.NEW_DRIVE_SIZE,
-            spaceUsed: 0,
-          },
-        })
-      ])
-      .then(() => {
-        // If a new account has been created successfully, create a new redis record with ttl=1h to limit signup requests to one per hour
-        client.set(req.headers['x-forwarded-for'] + '-signup', ' ');
-        client.expire(req.headers['x-forwarded-for'] + '-signup', 3600);
-  
-        return res.sendStatus(201)
-      })
-      .catch(() => {
-        return res.sendStatus(409)
-      })
-    } catch {
-      return res.sendStatus(500);
-    }   
+    await createAll()
+    .then(() => {
+      client.set(req.headers['x-forwarded-for'] + '-signup', ' ');
+      client.expire(req.headers['x-forwarded-for'] + '-signup', 3600);
+      return res.sendStatus(201);
+    })
+    .catch(() => {
+      return res.sendStatus(409);
+    })
   } else {
     return res.sendStatus(423);
   }
