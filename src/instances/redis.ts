2ca221f953b837a0645b85a1e0e7ba3642f24936
StingrayFG <54187585+StingrayFG@users.noreply.github.com>
2025-01-02 11:20:42 +0100

    Update: fixed redis client closing connection

--- src/instances/redis.ts
+++ src/instances/redis.ts
@@ -3,16 +3,17 @@ import { createClient } from 'redis'
 /* Due to the lib used to mock redis being broken, redis client cant be mocked for testing.
 Not creating a redis client at all during the testing using IGNORE_AUTH_LIMIT environment
 variable is the solution used here */
-export const instance = (!process.env.IGNORE_AUTH_LIMIT) ?
-  createClient({ url: process.env.REDIS_URL })
-  :
-  {
+let client: any;
+
+if (process.env.IGNORE_AUTH_LIMIT) {
+  client = createClient({ url: process.env.REDIS_URL, legacyMode: true })
+  client.connect();
+} else {
+  client = {
     set: (name: string, value: any) => {},
     get: (name: string) => {},
     expire: (name: string, period: number) => {}
   }
+}
 
-  
-
-
-
+export const instance = client;
